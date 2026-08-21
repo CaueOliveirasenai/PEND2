dcf4cc7f4fb31177c2284328301882adcdd29971
CaueOliveirasenai <caue.h.oliveira@aluno.senai.br>
2026-08-21 08:27:11 -0300

    desafio extra

--- O.O/O.O+DOM/Atividade Prática/script.js
+++ O.O/O.O+DOM/Atividade Prática/script.js
@@ -7,13 +7,14 @@ class Produto {
         this.desconto = desconto;
     }
 
-    //Métodos
+    // Métodos
 
     aplicarDesconto() {
         this.preco = this.preco - (this.preco * this.desconto / 100);
     }
 
 }
+
 
 class Estoque {
 
@@ -25,22 +26,41 @@ class Estoque {
         this.produtos.push(produto);
     }
 
+    excluirProduto(indice) {
+        this.produtos.splice(indice, 1);
+        this.exibirNaTela();
+    }
+
     exibirNaTela() {
+
         const resultado = document.querySelector("#resultado");
 
         resultado.innerHTML = "";
 
-        this.produtos.forEach(produto => {
+        this.produtos.forEach((produto, indice) => {
+
             resultado.innerHTML += `
-                <p>Nome: ${produto.nome}</p>
-                <p>Preço: ${produto.preco}</p>
-                <p>Categoria: ${produto.categoria}</p>
-                <p>Desconto: ${produto.desconto}%</p>
+                <div class="produto">
+
+                    <p>Nome: ${produto.nome}</p>
+                    <p>Preço: R$ ${produto.preco}</p>
+                    <p>Categoria: ${produto.categoria}</p>
+                    <p>Desconto: ${produto.desconto}%</p>
+
+                    <button onclick="estoque.excluirProduto(${indice})">
+                        Excluir
+                    </button>
+
+                    <hr>
+
+                </div>
             `;
-        })
+
+        });
     }
-    
+
 }
+
 
 const estoque = new Estoque();
 
@@ -48,14 +68,23 @@ const nome = document.querySelector("#nome");
 const preco = document.querySelector("#preco");
 const categoria = document.querySelector("#categoria");
 const desconto = document.querySelector("#desconto");
+
 const botaoCadastrar = document.querySelector("#botaoCadastrar");
 
 
 botaoCadastrar.addEventListener("click", function() {
 
-    const produto = new Produto(nome.value, preco.value, categoria.value, desconto.value);
+    const produto = new Produto(
+        nome.value,
+        preco.value,
+        categoria.value,
+        desconto.value
+    );
 
     produto.aplicarDesconto();
+
     estoque.adicionarProduto(produto);
+
     estoque.exibirNaTela();
+
 });
